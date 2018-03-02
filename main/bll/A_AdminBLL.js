52864dde32398ae3b0e550d7837632d2fd4d0a32
Lance CHEN <946304724@qq.com>
2018-03-02 19:38:01 +0800

    Initial commit

---
+++ main/bll/A_AdminBLL.js
@@ -0,0 +1,8 @@
+// const allUser = require('../dao/AllUser')
+// const otherUser = require('../dao/OtherUser')
+// const administrator = require('../dao/Administrator')
+// const a_admin = require('../dao/A_Admin')
+//
+// function addA_Admin() {
+//
+// }
---
+++ main/util/db_manager.js
@@ -0,0 +1,10 @@
+// 数据库连接配置
+module.exports = {
+  mysql: {
+    host: '202.113.31.101',
+    user: 'root',
+    password: '123456',
+    database: 'volunteerservice',
+    port: '13306'
+  }
+};
