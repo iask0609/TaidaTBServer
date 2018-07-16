c36de30445ff97092837abf05278767e7269f090
lzh <nkulzh16@gmail.com>
2018-07-16 12:32:24 +0800

    fix some bug

--- main/blockchain/voteForApplication.js
+++ main/blockchain/voteForApplication.js
@@ -5,7 +5,7 @@ function voteForApplication(userID,userAddress, contractHash, score1, score2, sc
     node = Node(userID);
     node.start((web3, child)=>{
         var contract = new web3.eth.Contract(config.abiDefinition, contractHash);
-        web3.eth.personal.unlockAccount(UserAddress,'123456', 10000).then((response)=>{
+        web3.eth.personal.unlockAccount(userAddress, '123456', 10000).then((response) => {
             contract.methods.scoring(score1, score2, score3, score4).send({
                 from: userAddress
             }).then((reciept)=>{
